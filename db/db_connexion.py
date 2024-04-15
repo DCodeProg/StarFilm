ef03d246341d2fc2786d72a82819747465c11f5e
MathissGit <mathissineux@gmail.com>
2024-04-15 16:17:15 +0200

    build: connexion with db_StarFilm.db

--- db/db_connexion.py
+++ db/db_connexion.py
@@ -1,9 +1,5 @@
 from sqlalchemy import create_engine
 from sqlalchemy import select
-from sqlalchemy.orm import Session
-
-from model import *
-
 
 db_path = r'sqlite:///db/db_StarFilm.db'
 
@@ -11,11 +7,7 @@ engine = create_engine(db_path)
 
 try: 
     conn = engine.connect()
-    print("Success!")
-
-    session = Session(engine)
-    stmt = select(Users)
+    # print("Success!")
     
 except Exception as ex: 
     print(ex)
-
