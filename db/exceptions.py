b5211da6544545e4d504b17d3a82d713e767c1b6
MathissGit <mathissineux@gmail.com>
2024-04-15 20:12:32 +0200

    Build: Creating Exceptions

---
+++ db/exceptions.py
@@ -0,0 +1,27 @@
+class ErrorListFavorites(Exception):
+    pass
+
+class AlreadyOnFavorite(Exception):
+    pass
+
+class NotConnected(Exception):
+    pass
+
+class ErrorAddingFavorite(Exception):
+    pass
+
+class ErrorDeleteFavorite(Exception): 
+    pass
+
+class ErrorQueryDeleteFavorite(Exception): 
+    pass
+
+class NotAdmin(Exception):
+    pass
+
+class FavoriteNotFind(Exception):
+    pass 
+
+class ErrorRecoveryFavorites(Exception):
+    pass
+
